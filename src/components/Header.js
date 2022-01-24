367aa6f0727f2f118469bdd5b0ac335d6babaa16
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-01-24 06:58:20 +0200

    add routes and main components
    add Header, Search, Shelves components,
    
    add home and search routes,
    
    remove the state used to change the layout and link the buttons to the routes, to change the layout instead of the state.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+  <div>
+      <div className="list-books-title">
+      <h1>MyReads</h1>
+      </div>
+  </div>
+  )
+}
---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export default function Search() {
+  return (
+    <div>
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link to='/' className="close-search">Close</Link>
+              <div className="search-books-input-wrapper">
+                {}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+    </div>
+  )
+}
